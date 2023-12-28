a51c594cef6baa1378988990e7c036b141fe4e85
edpomacedo <edpomacedo@gmail.com>
2023-12-27 22:22:18 -0300

    v0.1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 
 function App() {
+  const [serviceStatus, setServiceStatus] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const checkServiceStatus = async (url, serviceName) => {
+    try {
+      const response = await fetch(url);
+      const status = response.status === 404 ? 'Operacional' : 'Indisponível';
+      setServiceStatus(prevStatus => ({ ...prevStatus, [serviceName]: status }));
+    } catch (error) {
+      setServiceStatus(prevStatus => ({ ...prevStatus, [serviceName]: 'Erro ao verificar o status' }));
+    }
+  };
+
+  useEffect(() => {
+    // Função para verificar o status dos serviços
+    const checkStatus = async () => {
+      setLoading(true);
+
+      // Verificar o status inicial
+      await checkServiceStatus('https://proxy.bdij.com.br', 'Proxy');
+      // await checkServiceStatus('https://api.bdij.com.br', 'API');
+      // Adicione mais serviços conforme necessário
+
+      setLoading(false);
+    };
+
+    // Executar a verificação inicial
+    checkStatus();
+
+    // Configurar verificação a cada minuto
+    const intervalId = setInterval(() => {
+      checkStatus();
+    }, 60000); // 60000 milissegundos = 1 minuto
+
+    // Limpar o intervalo ao desmontar o componente
+    return () => clearInterval(intervalId);
+  }, []); // O segundo parâmetro [] garante que o useEffect só é executado uma vez no montar do componente
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Status dos Serviços</h1>
+
+      {/* Renderizar a mensagem de "Aguardando" ou o spinner durante o carregamento */}
+      {loading && <div>Aguardando...</div>}
+
+      {/* Renderizar o status dos serviços após o carregamento */}
+      {!loading && Object.entries(serviceStatus).map(([serviceName, status]) => (
+        <div key={serviceName} className={`service-status ${status === 'Operacional' ? 'service-operational' : 'service-down'}`}>
+          {serviceName}: {status}
+        </div>
+      ))}
     </div>
   );
 }
